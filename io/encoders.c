e1f1d8b34a5c7b41ce584a1eb8e5ffbc15fcb99c
Kresimir Popovic <kresimir@noisesculptors.com>
2026-06-13 10:12:17 +0200

    temporary changes in encoders.c

--- io/encoders.c
+++ io/encoders.c
@@ -23,13 +23,11 @@ void io_encoders_init(void) {
     *RCC_AHB4ENR |= (1u<<GPIOAEN);
 
 #define TIM2EN  0
-#define TIM15EN 16
     *RCC_APB1LENR |= (1u<<TIM2EN);
-    *RCC_APB2ENR |= (1u<<TIM15EN);
 
-    gpio_ctrl(GPIOA, GPIO_MODE, GPIO0|GPIO1|GPIO2|GPIO3, MODE_AF);
+    gpio_ctrl(GPIOA, GPIO_MODE, GPIO0|GPIO1, MODE_AF);
+    gpio_ctrl(GPIOA, GPIO_MODE, GPIO2|GPIO3, MODE_INPUT);
     gpio_ctrl(GPIOA, GPIO_AFRL, GPIO0|GPIO1, AF1); // TIM2 CH1 CH2
-    gpio_ctrl(GPIOA, GPIO_AFRL, GPIO2|GPIO3, AF4); // TIM15 CH1 CH2
 
 #define CC2E 4
 #define CC1E 0
@@ -43,26 +41,19 @@ void io_encoders_init(void) {
 #define CC2S 8
 #define CC1S 0 
     /*
-       TIM2 & TIM3 in new revision
+       TIM2 & TIM3 in new revision - TIM15 has unfortunately no encoder hw so
+       software solution is used before new revision with TIM3 is manufactured
      */
-    // Configure TIM2 and TIM15 for Encoder Input Mode
     *TIM2_SMCR |= (3u<<SMS);  // Encoder mode 3: Counts on both TI1 and TI2
     *TIM2_CCMR1 &= ~0xFFFF;
     // Set channels as input mapped to TI1 and TI2
     *TIM2_CCMR1 |= (2u<<CC2S) | (2u<<CC1S);
     *TIM2_ARR = 0xFFFF;
 
-    *TIM15_SMCR |= (3u<<SMS);
-    *TIM15_CCMR1 &= ~0xFFFF;
-    *TIM15_CCMR1 |= (2u<<CC2S) | (2u<<CC1S);
-    *TIM15_ARR = 0xFFFF;
-
     /* Enabe timers */
 #define CEN 0
     *TIM2_CR1 |= (1u<<CEN);
     *TIM2_CNT = 1;
-    *TIM15_CR1 |= (1u<<CEN);
-    *TIM15_CNT = 1;
 }
 
 io_enc_t io_enc_read(size_t idx)
@@ -72,8 +63,27 @@ io_enc_t io_enc_read(size_t idx)
 
     switch(idx){
         case 0:
-            now = (int16_t)*TIM15_CNT;    // encoder position (16-bit)
-            d16 = (int16_t)(now - last0); // wrap-safe signed delta
+            now = (int16_t)(*GPIOA_IDR & 0xc)>>2;
+            switch(now){
+                case 0:
+                    if(last0==1) d16++;
+                    if(last0==2) d16--;
+                    break;
+                case 1: 
+                    if(last0==3) d16++;
+                    if(last0==0) d16--;
+                    break;
+                case 2:
+                    if(last0==0) d16++;
+                    if(last0==3) d16--;
+                    break;
+                case 3:
+                    if(last0==2) d16++;
+                    if(last0==1) d16--;
+                    break;
+            }
+           //  rising:  02310
+           //  falling: 01320
             last0 = now;
              break;
         case 1:
