415d5e9556af4d13b04d5b71a454537beae2a356
Kresimir Popovic <kresimir@noisesculptors.com>
2026-06-22 14:03:01 +0200

    updated lib/noisesculptors-core lib/printf

--- lib/noisesculptors-core/include/h750/flash.h
+++
@@ -1,9 +0,0 @@
-#ifndef FLASH_H
-#define FLASH_H
-
-#include "memory.h"
-#include <stdint.h>
-
-#define FLASH_ACR    (volatile uint32_t *)(FLASH + 0x0)
-
-#endif
--- lib/noisesculptors-core/include/h750/vtor.h
+++
@@ -1,8 +0,0 @@
-#ifndef VTOR_H
-#define VTOR_H
-
-#include <stdint.h>
-
-#define SCB_VTOR  (volatile uint32_t *)0xE000ED08U
-
-#endif
